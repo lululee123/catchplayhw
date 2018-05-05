a0962aa3d7b2681d46d59a735865ca324407e510
LuluLee <lulululu123456123456@gmail.com>
2018-05-05 17:53:07 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+//web warn
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var sidebtn = document.getElementById("sidemenu__warn-btn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+sidebtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+//mobile warn
+var sidemenu__modal = document.getElementById('sidemenu__myModal');
+var sidemenu__btn = document.getElementById("sidemenu");
+var sidemenu__span = document.getElementsByClassName("sidemenu__close")[0];
+sidemenu__btn.onclick = function() {
+    sidemenu__modal.style.display = "block";
+}
+sidemenu__span.onclick = function() {
+    sidemenu__modal.style.display = "none";
+}
+
+//btn
+$('.button__watch').click(function(){
+  $('html,body').animate({scrollTop:$('.downtitle').offset().top},800);
+});
+
+//slider
+// $('.center').slick({
+//     centerMode: true,
+//     infinite: true,
+//     centerPadding: '60px',
+//     slidesToShow: 3,
+//     speed: 500,
+// });
+$('.center').slick({
+    centerMode: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '6em',
+          slidesToShow: 1, 
+        }
+      }, 
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '2em',
+          slidesToShow: 1, 
+        }
+      }
+    ]
+  });
+ 
